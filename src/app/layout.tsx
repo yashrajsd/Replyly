ac5b65c5ffb3b08ad8e16df4abfb686637e6e139
Yashraj Deshmukh <yashrajdeshmukh@eng.rizvi.edu.in>
2024-12-19 12:23:57 +0530

    clerk initial setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
+import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import {
+  ClerkProvider,
+} from '@clerk/nextjs'
+import { ThemeProvider } from "@/providers/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +19,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider >
+      <html lang="en">
+        <body className={inter.className}>
+          <ThemeProvider
+              attribute="class"
+              defaultTheme="system"
+              disableTransitionOnChange
+            >
+          {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
